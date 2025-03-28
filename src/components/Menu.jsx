26dddcf3df68f5b1aabb82cabeefeea821b4cd7a
Shreyas0017 <shreyassaha00@gmail.com>
2025-03-28 22:22:01 +0530

    Glitch fixed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
+import { motion } from 'framer-motion';
 import { FiShoppingCart, FiFilter, FiSearch } from "react-icons/fi";
 import { db } from "../config"; // Firebase config file
 import { collection, getDocs } from "firebase/firestore";
